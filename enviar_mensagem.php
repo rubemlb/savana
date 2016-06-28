82cc433181cd7eaf7328bd8cedb149d3cac56aba
rubemlb <rubemlbarros@gmail.com>
2016-06-28 16:05:31 -0100

    v1.0
    map_email

---
+++ enviar_mensagem.php
@@ -0,0 +1,144 @@
+<?php
+
+$expected = ['nome','telefone','email','assunto','mensagem'];
+$required = ['nome','telefone','email','assunto','mensagem'];
+
+// check $_POST array
+foreach ($_POST as $key => $value) {
+    if (in_array($key, $expected)) {
+        if (!is_array($value)) {
+            $value = trim($value);
+        }
+        if (empty($value) && in_array($key, $required)) {
+            $$key = '';
+            $missing[] = $key;
+        } else {
+            $$key = $value;
+        }
+    }
+}
+
+// check email address
+if (!in_array($email, $missing)) {
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    if (!$email) {
+        $errors['email'] = 'Please use a valid email address';
+    }
+}
+
+
+// process only if there are no errors or missing fields
+if (!$errors && !$missing) {
+    require_once 'config.php';
+
+    // set up replacements for decorator plugin
+    $replacements = [
+        'reservasavana@yahoo.com' =>
+            ['#subject#' => 'Contact Hotel Savana',
+                '#greeting#' => "$nome has sent you a message, see content below"]
+   ];
+
+    try {
+        // create a transport
+        $transport = Swift_SmtpTransport::newInstance($smtp_server, 465, 'ssl')
+            ->setUsername($username)
+            ->setPassword($password);
+        $mailer = Swift_Mailer::newInstance($transport);
+
+        // register the decorator and replacements
+        $decorator = new Swift_Plugins_DecoratorPlugin($replacements);
+        $mailer->registerPlugin($decorator);
+
+        // initialize the message
+        $message = Swift_Message::newInstance()
+            ->setSubject('#subject#')
+            ->setFrom($from);
+
+        $image_ilha = $message->embed(Swift_Image::fromPath('img/fogo.png'));
+        $image_logo = $message->embed(Swift_Image::fromPath('img/logo.png'));
+		
+        // create the first part of the HTML output
+        $html = <<<EOT
+<html lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>Hotel Savana</title>
+</head>
+<body bgcolor="#EBEBEB" link="#B64926" vlink="#FFB03B">
+<table width="100%" border="0" cellspacing="0" cellpadding="0" bgcolor="#EBEBEB">
+<tr>
+<td>
+<table width="600" align="center" border="0" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
+<tr><td style='text-align:center; background-color: grey'><img src='$image_logo'></td></tr>
+<tr>
+<td style="padding-top: 0.5em">
+<h1 style="font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif; color: #0E618C; text-align:
+center">Contact - Hotel Savana</h1>
+</td>
+</tr>
+<tr>
+<td style="font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif; color: #1B1B1B; font-size: 14px; padding: 1em">
+<p>#greeting#</p>
+<ul style="list-style-type: none">
+EOT;
+
+        // initialize variable for plain text version
+        $text = '';
+
+        // add each form element to the HTML and plain text content
+        foreach ($expected as $item) {
+            if (isset($$item)) {
+                $value = $$item;
+                $label = ucwords(str_replace('_', ' ', $item));
+                $html .= "<li>> $label: ";
+                if (is_array($value)) {
+                    $value = implode(', ', $value);
+                }
+                $html .= "$value</li>";
+                $text .= "$label: $value\r\n";
+            }
+        }
+
+        // complete the HTML content
+        $html .= '</ul></td></tr>';
+        $html .="<tr><td style='text-align:center'><img src='$image_ilha'></td></tr>";
+        $html .= '</table></td></tr></table></body></html>';
+
+        // set the HTML body and add the plain text version
+        $message->setBody($html, 'text/html')
+            ->addPart($text, 'text/plain');
+
+        // initialize variables to track the emails
+        $sent = 0;
+        $failures = [];
+
+        // send the messages
+        foreach ($replacements as $recipient => $values) {
+            $message->setTo($recipient);
+            $sent += $mailer->send($message, $failures);
+        }
+
+        // if the message have been sent, redirect to relevant page
+        if ($sent == 1) {
+            header('Location: contact_sucess.html');
+            exit;
+        }
+
+// handle failures
+        $num_failed = count($failures);
+        if ($num_failed == 1) {
+            $f = 'both';
+        } elseif (in_array($email, $failures)) {
+            $f = 'email';
+        } else {
+            $f = 'reg';
+        }
+
+// IMPORTANT: log an error before redirecting
+
+        header("Location: error.php?f=$f");
+        exit;
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
---
+++ thanks.php
@@ -0,0 +1,222 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Casa Strela B&B | Hotel</title>
+
+
+
+<!-- Google fonts -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
+
+<!-- font awesome -->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+<link rel="stylesheet" href="jquery/jRating.jquery.css"/>
+
+<!-- uniform --><!-- Google fonts -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
+
+<!-- font awesome -->
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+<!-- animate.css -->
+<link rel="stylesheet" href="assets/wow/animate.css" />
+
+<!-- uniform -->
+<link type="text/css" rel="stylesheet" href="assets/uniform/css/uniform.default.min.css" />
+
+<!-- gallery -->
+<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+
+<script type="text/javascript">
+        function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+        }
+    </script>
+
+
+<!-- favicon -->
+
+<link rel="icon" href="images/favicon.ico">
+
+<link rel="stylesheet" href="assets/style.css">
+
+<script src='../../google_analytics_auto.js'></script></head>
+
+<body id="home">
+
+
+<!-- top 
+  <form class="navbar-form navbar-left newsletter" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Enter Your Email Id Here">
+        </div>
+        <button type="submit" class="btn btn-inverse">Subscribe</button>
+    </form>
+ top -->
+
+<!-- header -->
+<nav class="navbar  navbar-default" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html"><img src="images/logo2.png"  alt=""></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+      
+      <ul class="nav navbar-nav"> 
+        <li><a href="introduction.html">Introduction</a></li> 
+        <li><a href="services.html">Services</a></li>
+        <li><a href="rooms-tariff.html">Rooms & Tariff</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="reserve.html">Reserve</a></li>       
+        <li><a href="satisfaction.html">Satisfaction</a></li>
+        <li><a href="contact.html">Contact</a></li>  
+      </ul>
+    </div><!-- Wnavbar-collapse -->
+  </div><!-- container-fluid -->
+</nav>
+
+<div class="container">
+
+
+<div class="contact">
+
+  <div class="row">
+        
+        <div class="col-sm-12">
+
+          <h2>Evaluation</h2>
+            <p>Thank you for the evaluation. </p>
+
+       </div>
+</div>
+</div>
+<!-- form -->
+
+</div>
+
+
+
+
+<footer class="spacer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5">
+                    <h4>Casa Strela</h4>
+                    <p>Casa Strela B&B - Tarrafal is located in Tarrafal. Free WiFi access is available. Extras include a fan. At Casa Strela B&B - Tarrafal you will find a garden, barbecue facilities and a terrace. Other facilities offered at the property include water sports facilities, a shared lounge and a tour desk. An array of activities can be enjoyed on site or in the surroundings, including cycling, fishing and hiking. The property offers free parking. </p>
+                </div>              
+                 
+                 <div class="col-sm-3">
+                    <h4>Quick Links</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="introduction.html">Introduction</a></li> 
+                        <li><a href="services.html">Services</a></li>
+                        <li><a href="rooms-tariff.html">Rooms & Tariff</a></li>
+                        <li><a href="gallery.html">Gallery</a></li>
+                        <li><a href="reserve.html">Reserve</a></li>       
+                        <li><a href="satisfaction.html">Satisfaction</a></li>
+                        <li><a href="contact.html">Contact</a></li>  
+                    </ul>
+                </div>
+                 <div class="col-sm-4 subscribe">
+                    <h4>Stay Connected</h4>
+                    <div class="social">
+                    <a href="#"><i class="fa fa-facebook-square" data-toggle="tooltip" data-placement="top" data-original-title="facebook"></i></a>
+                    <a href="#"><i class="fa fa-twitter-square" data-toggle="tooltip" data-placement="top" data-original-title="twitter"></i></a>
+                    </div>
+                </div>
+            </div>
+            <!--/.row--> 
+        </div>
+        <!--/.container-->    
+    
+    <!--/.footer-bottom--> 
+</footer>
+
+<div class="text-center copyright">Powered by <a href="#">Prime 2016</a></div>
+
+
+
+<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title">title</h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->    
+</div>
+
+
+<script src="assets/jquery.js"></script>
+
+<script type="text/javascript" src="jquery/jRating.jquery.js"></script>
+
+
+    <script type="text/javascript">
+      
+        $(document).ready(function(){
+
+          $(".rating").jRating({
+
+            decimalLength : 1,
+            rateMax : 5,
+            PhpPath:'process.php'
+          });
+        })
+    </script>
+
+<!-- wow script -->
+<script src="assets/wow/wow.min.js"></script>
+
+<!-- uniform -->
+<script src="assets/uniform/js/jquery.uniform.js"></script>
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="assets/mobile/touchSwipe.min.js"></script>
+
+<!-- jquery mobile -->
+<script src="assets/respond/respond.js"></script>
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- gallery -->
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+<!-- custom script -->
+<script src="assets/script.js"></script>
+
+<script type="text/javascript" src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+</body>
+
+
+</html>
